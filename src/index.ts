b42bbaf7d0eefb12fe29cd156d0dc3a2ce410830
Rhuan Victor Virtudes Lourenco <rhvirtudes1@gmail.com>
2022-11-18 01:21:10 -0300

    Endpoint de pagar contas

--- src/index.ts
+++ src/index.ts
@@ -64,15 +64,16 @@ app.post("/accounts/newAccount", (req: Request, res: Response) => {
 
         if (!validateAge(Number(separateDate[0]), Number(separateDate[1]), Number(separateDate[2]))) {
             res.status(400).send('Conta não criada. É necessário ter 18 anos ou mais')
-        }else{ accounts.push({
-            id: accounts.length + 1,
-            name: name,
-            cpf: cpf,
-            birthDate: birthDate,
-            balance: 0
-        
-        })
-    }
+        } else {
+            accounts.push({
+                id: accounts.length + 1,
+                name: name,
+                cpf: cpf,
+                birthDate: birthDate,
+                balance: 0
+
+            })
+        }
         res.status(200).send('Conta criada com sucesso')
 
     } catch (error: any) {
@@ -136,19 +137,90 @@ app.put("/accounts/account", (req: Request, res: Response) => {
         accounts.map((account) => {
             if (account.name === name && account.cpf === cpf) {
                 account.balance = account.balance + value
-                 userBalance = {
+                userBalance = {
                     name: account.name,
                     cpf: account.cpf,
                     birthDate: account.birthDate,
                     balance: account.balance
                 }
-            
-            return userBalance
-    }})
-        
+
+                return userBalance
+            }
+        })
+
         res.status(200).send(userBalance)
-        
+
     } catch (error: any) {
         res.status(errorCode).send(error.message)
     }
+})
+
+//Endpoint que paga conta
+
+app.patch("/accounts/:id/payTheBills", (req: Request, res: Response) => {
+
+    try {
+
+        const user = Number(req.params.id)
+        let { value, description, payday } = req.body
+
+        if (!value || !description) {
+            errorCode = 422
+            throw new Error("Passe os parametros corretamente");
+        }
+
+        if (typeof (value) !== "number") {
+            errorCode = 422
+            throw new Error("Valor inválido");
+        }
+
+        if (typeof (description) !== "string") {
+            errorCode = 422
+            throw new Error("Descrição inválido");
+        }
+
+        let d = new Date();
+        d.setHours(0,0,0,0)
+
+        let currentYear = d.getFullYear()
+        let currentMonth = d.getMonth() + 1
+        let currentDay = d.getDate()
+
+        if(!payday) {
+            payday = `${currentYear}/${currentMonth}/${currentDay}`
+        }
+
+        const separatePayday = payday.split("/")
+
+        const findUser = accounts.find((current: Account) => {
+            return current.id === user
+        })
+
+        if (findUser) {
+            //usuario tem saldo
+            if (value > findUser.balance) {
+                res.status(400).send("Saldo insuficiente")
+            } else {
+                //payday válido
+                const date = new Date(`${separatePayday[2]}-${separatePayday[1]}-${separatePayday[0]} 00:00:00`);
+
+                if (date < d) {
+                   return res.status(400).send("Data inválido")
+                } else if(date > d) {
+                    findUser.balance = findUser.balance - value
+                   return res.status(200).send("Agendamento Realizado com sucesso.")
+                }
+                findUser.balance = findUser.balance - value
+                
+                return res.status(200).send("Conta paga")
+            }
+
+        } else {
+            res.status(404).send("Usuário não encontrado")
+        }
+
+    } catch (error: any) {
+        res.status(errorCode).send(error.message)
+    }
+
 })
